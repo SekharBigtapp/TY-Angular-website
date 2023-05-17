44753a0153d329a0245c43158e7efb4c589d150d
Karthikeyan-FullStack-Developer <karthikeyan.d@bigtappanalytics.net>
2023-05-17 15:21:43 +0530

    donations

--- src/app/module/donations/donations.component.ts
+++ src/app/module/donations/donations.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { LandingPageService } from 'src/app/core/services/landing_page/landing-page.service';
 
 @Component({
   selector: 'app-donations',
@@ -8,13 +10,17 @@ import { Router } from '@angular/router';
 })
 export class DonationsComponent implements OnInit {
   disabledAgreement: boolean = false;
-  constructor(private router: Router) { }
+  constructor(private router: Router,private service: LandingPageService, private formBuilder: FormBuilder) { }
 
   isShow: boolean = true;
   topPosToStartShowing = 100;
-  
+  touchForm!: FormGroup
+  countryList: any = []
 
-  @HostListener('window:scroll')
+  errorMessage: any = undefined
+  messageSent: boolean = false
+
+  // @HostListener('window:scroll')
 
 
 
@@ -50,8 +56,67 @@ export class DonationsComponent implements OnInit {
       $("#navbar").toggleClass("navbar-mobile");
       e.preventDefault();
     });
+
+    this.touchForm = this.formBuilder.group({
+      name: [null, Validators.compose([Validators.required])],
+      email: [null, Validators.compose([Validators.email, Validators.required])],
+      phoneNumber: [null, Validators.compose([Validators.required, Validators.pattern(/^[0-9]{10}$/)])],
+      country: [null, Validators.compose([Validators.required])],
+      query: [null, Validators.compose([Validators.required])]
+    })
+    this.getCountryDetails()
+  }
+  getCountryDetails() {
+    this.service.countryList().subscribe({
+      next: (response: any) => {
+        this.countryList = response
+      },
+      error: (error: any) => {
+        console.error(error);
+
+      }
+    })
   }
 
+  formatCamelCase(value: any) {
+    return value && value.charAt(0).toUpperCase() + value.slice(1)
+  }
+
+
+  submitTouch() {
+    this.errorMessage = undefined;
+    this.messageSent = false
+    const { name, country, query, phoneNumber, email } = this.touchForm.value
+
+    if (this.touchForm.invalid) {
+      // this.errorMessage = "Form is invalid"
+      console.log(this.errorMessage);
+      return this.touchForm.markAllAsTouched()
+    }
+    const body =
+
+    {
+      "name": this.formatCamelCase(name),
+      "emailId": email,
+      "mobileNumber": "+" + this.countryList.filter((ele: any) => ele.countryId == country)[0].dialCode + "-" + phoneNumber,
+      "country": {
+        "countryId": Number(country)
+      },
+      "query": this.formatCamelCase(query)
+    }
+
+    this.service.getInTouchAdd(body).subscribe({
+      next: (response: any) => {
+        this.touchForm.reset()
+        this.messageSent = true
+      },
+      error: (error) => {
+        console.error(error);
+
+      }
+
+    })
+  }
   isCheckboxenabled(event:any){
     console.log(event);
     let isChecked = event.target.checked;
