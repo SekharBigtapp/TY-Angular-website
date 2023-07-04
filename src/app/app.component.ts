70cb2da9858fc4e26a41b4400cda7cfb2f89e87d
Krishna Kishore Nana <kishore.n@bigtappanalytics.net>
2023-07-04 13:02:27 +0530

    Alert popup issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,19 +8,30 @@ import * as $ from 'jquery';
 })
 export class AppComponent implements OnInit{
   event : any;
-  @HostListener('window:beforeunload')
-  onBeforeUnload() {
-    return false;
-  }
+  // @HostListener('window:beforeunload')
+  // onBeforeUnload() {
+  //   return false;
+  // }
+
 
   ngOnInit(): void {
+    // document.onclick = (args: any): void => {
+    //   //alert();
+    //   let currentTarget = args.target.closest('nav');
+    //   if (currentTarget == null) {
+    //     console.log("Called");
+    //     $('.mobile-nav-toggle').removeClass("bi-x");
+    //     $("#navbar").removeClass("navbar-mobile");
+    //   }
+    // }
+
+
     $(document).on('ready', function() {
       $('.mobile-nav-toggle').on('click', function(e){
         $('.mobile-nav-toggle').toggleClass("bi-x");
         $("#navbar").toggleClass("navbar-mobile");
         e.preventDefault();
       });
-
     });
   }
 
